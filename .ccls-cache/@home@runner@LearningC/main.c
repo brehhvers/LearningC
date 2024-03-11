85a1ce2a3e638d4aff937c08f43d23773c2851b4
Brenda Evers <105288127+brehhvers@users.noreply.github.com>
2024-03-11 21:01:04 +0000

    New activities

--- .ccls-cache/@home@runner@LearningC/main.c
+++ .ccls-cache/@home@runner@LearningC/main.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  int result, fat = 1, num, c;
+  int c = 0, voto, vJ = 0, vM = 0, vN = 0, maior, vencedor;
+  float pvN;
 
-  printf("Fatorial do numero: ");
-  scanf("%d", &num);
+  while (c < 8) {
+    printf("Informe seu voto:\n");
+    printf("1 - Joao\n");
+    printf("2 - Maria\n");
+    printf("3 - Nulo\n");
+    printf("\nSua escolha: ");
+    scanf("%d", &voto);
 
-  result = num * (num - fat);
-  printf("%d x %d = %d\n", num, num-fat, result);
-
-  c = num - 1;
-  fat++;
-
-  while(c > 1){
-    printf("%d ", result);
-    result = result * (num - fat);
-    printf("x %d = %d\n", num-fat, result);
-
-    fat++;
-    c--;
+    switch (voto) {
+    case 1:
+      vJ++;
+      break;
+    case 2:
+      vM++;
+      break;
+    case 3:
+      vN++;
+      break;
+    }
+    printf("\n------------------\n");
+    c++;
   }
- 
 
-  
+  ((vJ) > (vM) ? (maior = vJ) : (maior = vM));
+  vencedor = maior;
+
+  pvN = (vN / 8.0) * 100;
+  // Nulo nao e um candidato.
+  printf("(%d)Joao  (%d)Maria  (%d)Nulo\n", vJ, vM, vN);
+  printf("Vencedor: (%d)\n", vencedor);
+  printf("(%.1f%%)Votos nulos\n", pvN);
 }
