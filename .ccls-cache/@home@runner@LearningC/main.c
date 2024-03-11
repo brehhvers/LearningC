e376aca96fca02c1b6404d0dfdc833eb33a09dc4
Brenda Evers <105288127+brehhvers@users.noreply.github.com>
2024-03-11 21:25:34 +0000

    Learning

--- .ccls-cache/@home@runner@LearningC/main.c
+++ .ccls-cache/@home@runner@LearningC/main.c
@@ -2,38 +2,39 @@
 #include <stdlib.h>
 
 int main(void) {
-  int c = 0, voto, vJ = 0, vM = 0, vN = 0, maior, vencedor;
-  float pvN;
+  int idade, gen, c = 0, nM = 0, nH = 0, sM = 0, somaPeso = 0, somaIdade = 0;
+  float peso, mH, mM, mediaPeso, mediaIdade, sH = 0;
 
-  while (c < 8) {
-    printf("Informe seu voto:\n");
-    printf("1 - Joao\n");
-    printf("2 - Maria\n");
-    printf("3 - Nulo\n");
-    printf("\nSua escolha: ");
-    scanf("%d", &voto);
+  while (c < 5) {
+    printf("Idade:");
+    scanf("%d", &idade);
+    printf("Genero(1-fem/2-masc):");
+    scanf("%d", &gen);
+    printf("Peso:");
+    scanf("%f", &peso);
+    printf("--------------------\n\n");
 
-    switch (voto) {
+    switch (gen) {
     case 1:
-      vJ++;
+      nM++;
+      sM += idade;
       break;
     case 2:
-      vM++;
-      break;
-    case 3:
-      vN++;
+      nH++;
+      sH += peso;
       break;
     }
-    printf("\n------------------\n");
+    somaPeso += peso;
+    somaIdade += idade;
     c++;
   }
+  mH = sH / nH;
+  mM = sM / nM;
+  mediaPeso = somaPeso / 5.0;
+  mediaIdade = somaIdade / 5.0;
 
-  ((vJ) > (vM) ? (maior = vJ) : (maior = vM));
-  vencedor = maior;
-
-  pvN = (vN / 8.0) * 100;
-  // Nulo nao e um candidato.
-  printf("(%d)Joao  (%d)Maria  (%d)Nulo\n", vJ, vM, vN);
-  printf("Vencedor: (%d)\n", vencedor);
-  printf("(%.1f%%)Votos nulos\n", pvN);
+  printf("(%d)Mulheres (%d)Homens\n", nM, nH);
+  printf("(%.2f)Media peso de homens\n", mH);
+  printf("(%.0f)Media idade de mulheres\n", mM);
+  printf("(%.2f)Media peso  (%.0f)Media idade\n", mediaPeso, mediaIdade);
 }
